0e9609ed6715e1ed4d0df38a1099eef8366d1f8b
Evo-32 <evodieumuhuza@gmail.com>
2026-04-23 19:30:36 +0200

    The average

---
+++ Exercises 2/Question1.js
@@ -0,0 +1,6 @@
+export default function calculateAverage(numbers) {
+  if (!numbers.length) return undefined;
+
+  const sum = numbers.reduce((acc, num) => acc + num, 0);
+  return sum / numbers.length;
+}
